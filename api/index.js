a2483ecd0f1539421037768d541836757a8aa23d
pallavi5011 <pallawi758@gmail.com>
2024-03-12 20:35:59 +0530

    update-user-api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,9 +15,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-  
+
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
