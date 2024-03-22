f6b93af0e8f53438d95bf8678467d6906848ffd5
pallavi5011 <pallawi758@gmail.com>
2024-03-22 11:40:43 +0530

    All fixed

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/VerifyUser.js';
 import {
   createComment,
   deleteComment,
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
+import { verifyToken } from '../utils/VerifyUser.js';
 import { create, deletepost, getposts, updatepost } from '../controllers/post.controller.js';
 
 const router = express.Router();
